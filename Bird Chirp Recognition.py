4fabedc1fa6aa765d6696c69682876dafc43ded2
AravindRagav1010 <82216765+AravindRagav1010@users.noreply.github.com>
2023-07-16 23:03:58 +0530

    Add files via upload

---
+++ Bird Chirp Recognition.py
@@ -0,0 +1,198 @@
+model:
+
+
+
+import json
+import os
+import math
+import librosa
+import warnings
+warnings.filterwarnings('ignore') # Never print matching warnings
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import tensorflow.keras as keras
+import joblib
+import matplotlib.pyplot as plt
+DATASET_PATH = "N:/hakathon/newdataset/"
+JSON_PATH = "N:/hakathon/processed/data.json"
+SAMPLE_RATE = 22050
+TRACK_DURATION = 30 # measured in seconds
+SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
+
+
+def save_mfcc(dataset_path, json_path, num_mfcc=40, n_fft=2048, hop_length=512, num_segments=5):
+    """Extracts MFCCs from music dataset and saves them into a json file along witgh genre labels.
+        :param dataset_path (str): Path to dataset
+        :param json_path (str): Path to json file used to save MFCCs
+        :param num_mfcc (int): Number of coefficients to extract
+        :param n_fft (int): Interval we consider to apply FFT. Measured in # of samples
+        :param hop_length (int): Sliding window for FFT. Measured in # of samples
+        :param: num_segments (int): Number of segments we want to divide sample tracks into
+        :return:
+        """
+
+    # dictionary to store mapping, labels, and MFCCs
+    data = {
+        "mapping": [],
+        "labels": [],
+        "mfcc": []
+    }
+
+    samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
+    num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
+
+    # loop through all genre sub-folder
+    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+
+        # ensure we're processing a genre sub-folder level
+        if dirpath is not dataset_path:
+
+            # save genre label (i.e., sub-folder name) in the mapping
+            semantic_label = dirpath.split("/")[-1]
+            data["mapping"].append(semantic_label)
+            print("\nProcessing: {}".format(semantic_label))
+
+            # process all audio files in genre sub-dir
+            for f in filenames:
+
+		# load audio file
+                file_path = os.path.join(dirpath, f)
+                signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
+
+                # process all segments of audio file
+                for d in range(num_segments):
+
+                    # calculate start and finish sample for current segment
+                    start = samples_per_segment * d
+                    finish = start + samples_per_segment
+
+                    # extract mfcc
+                    mfcc = librosa.feature.mfcc(signal[start:finish], sample_rate, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
+                    mfcc = mfcc.T
+
+                    # store only mfcc feature with expected number of vectors
+                    if len(mfcc) == num_mfcc_vectors_per_segment:
+                        data["mfcc"].append(mfcc.tolist())
+                        data["labels"].append(i-1)
+                        print("{}, segment:{}".format(file_path, d+1))
+
+    # save MFCCs to json file
+    with open(json_path, "w") as fp:
+        json.dump(data, fp, indent=4)
+        
+        
+if __name__ == "__main__":
+    save_mfcc(DATASET_PATH, JSON_PATH, num_segments=10)
+
+
+
+
+
+# path to json file that stores MFCCs and genre labels for each processed segment
+DATA_PATH =  "N:/hakathon/processed/data.json"
+
+def load_data(data_path):
+    """Loads training dataset from json file.
+        :param data_path (str): Path to json file containing data
+        :return X (ndarray): Inputs
+        :return y (ndarray): Targets
+    """
+
+    with open(data_path, "r") as fp:
+        data = json.load(fp)
+
+    # convert lists to numpy arrays
+    X = np.array(data["mfcc"])
+    y = np.array(data["labels"])
+
+    print("Data succesfully loaded!")
+
+    return X, y
+
+
+def plot_history(history):
+    """Plots accuracy/loss for training/validation set as a function of the epochs
+        :param history: Training history of model
+        :return:
+    """
+
+    fig, axs = plt.subplots(2)
+
+    # create accuracy sublpot
+    axs[0].plot(history.history["accuracy"], label="train accuracy")
+    axs[0].plot(history.history["val_accuracy"], label="test accuracy")
+    axs[0].set_ylabel("Accuracy")
+    axs[0].legend(loc="lower right")
+    axs[0].set_title("Accuracy eval")
+
+    # create error sublpot
+    axs[1].plot(history.history["loss"], label="train error")
+    axs[1].plot(history.history["val_loss"], label="test error")
+    axs[1].set_ylabel("Error")
+    axs[1].set_xlabel("Epoch")
+    axs[1].legend(loc="upper right")
+    axs[1].set_title("Error eval")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    # load data
+    X, y = load_data(DATA_PATH)
+
+    # create train/test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    # build network topology
+    model = keras.Sequential([
+
+        # input layer
+        keras.layers.Flatten(input_shape=(X.shape[1], X.shape[2])),
+
+        # 1st dense layer
+        keras.layers.Dense(512, activation='relu'),
+        keras.layers.Dropout(0.01),
+
+        # 2nd dense layer
+        keras.layers.Dense(256, activation='relu'),
+        keras.layers.Dropout(0.01),
+
+        # 3rd dense layer
+        keras.layers.Dense(64, activation='relu'),
+        keras.layers.Dropout(0.01),
+
+        # output layer
+        keras.layers.Dense(75, activation='softmax')
+    ])
+
+    # compile model
+    optimiser = keras.optimizers.Adam(learning_rate=0.0001)
+    model.compile(optimizer=optimiser,
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    # train model
+    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), batch_size=32, epochs=100)
+
+    # plot accuracy and error as a function of the epochs
+    plot_history(history)
+
+
+
+test_accuracy=model.evaluate(X_test,y_test,verbose=0)
+print(test_accuracy[1])
+
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+# Save the model.
+with open('model.tflite', 'wb') as f:
+  f.write(tflite_model)
+
+
+
